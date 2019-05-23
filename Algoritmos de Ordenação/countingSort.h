3bb7a22aa9d0b81d573d3b4174785d5e3f055e1b
Mariana Ferreira Rocha <marianaferrocha@gmail.com>
2019-05-23 12:45:33 -0300

    update

--- Algoritmos de Ordenação/countingSort.h
+++ Algoritmos de Ordenação/countingSort.h
@@ -4,8 +4,7 @@ int *countingSort(int* a, int k, int tamanhoVetorA){
 	
 	int *b=(int*)malloc(sizeof(int)*(tamanhoVetorA));
 	int i,j;
-	int c[k+1];//Ex:. Se 20 for o maior, o vetor só vai até o 19, entao o +1 é p ele ter o 20 tambem
-		   //assim vc pode usar o c[a[i]] corretamente lá embaixo
+	int c[k+1];
 
 	for(i=0 ; i<k+1 ; i++){
 		c[i]=0;
@@ -24,7 +23,7 @@ int *countingSort(int* a, int k, int tamanhoVetorA){
 
 	
 	for(j = tamanhoVetorA-1 ; j>=0 ; j--){
-		b[c[a[j]]-1] = a[j]; //b não tinha a posicao que chamava na primeira iteração
+		b[c[a[j]]-1] = a[j]; 
 
 		(c[a[j]])--;
 	}
